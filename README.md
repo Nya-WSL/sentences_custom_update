b2665ec03c824503dbd9f4cd8c84b2b1813f9ecb
狐日泽 <shdocter@nya-wsl.com>
2023-06-26 20:00:47 +0800

    v1.0.0-dev1

--- README.md
+++ README.md
@@ -1,33 +1,9 @@
 # Sentences Custom Update
 
-![python](https://img.shields.io/badge/Version-1.4.0-cyan) ![python](https://img.shields.io/badge/Python-3.11.3-blue) ![os](https://img.shields.io/badge/OS-remote|Windows-orange) ![os](https://img.shields.io/badge/OS-local|All-orange)
+![python](https://img.shields.io/badge/Version-1.0.0-cyan) ![python](https://img.shields.io/badge/Python-3.8.10-blue) ![os](https://img.shields.io/badge/OS-remote|Windows-orange) ![os](https://img.shields.io/badge/OS-local|All-orange)
 
 bot插件扩展，用于快速更新语录库
 
 ## 如何使用
 
-#### scu_local
-
-- 安装并运行pm2
-
-```
-cd /root/hitokoto-api
-npm i -g pm2
-pm2 startup
-pm2 start ecosystem.config.js --env production
-pm2 save
-```
-
-- 运行程序
-
-`python3 scu_local.py`
-
-#### scu_remote
-
-- Windows
-
-`scu_remote.exe`
-
-- Linux
-
-`python3 scu_remote.py # 因为scu_remote是为Windows设计的，不推荐在Linux中使用>= 1.2.1版本的scu_remote`
+#### scu_web
--- scu_web.py
+++ scu_web.py
@@ -1,22 +1,31 @@
 import PySimpleGUIWeb as sg
 
+def Upload():
+    print()
+
 # Define the window's contents
-layout = [[sg.Text("What's your name?")],
-          [sg.Input(key='-INPUT-')],
-          [sg.Text(size=(40,1), key='-OUTPUT-')],
-          [sg.Button('Ok'), sg.Button('Quit')]]
+layout = [[sg.Text("语录名称")],
+          [sg.Input(key='-SentenceName-')],
+          [sg.Text("语录作者")],
+          [sg.Input(key='-SentenceAuthor-')],
+          [sg.Text("语录内容")],
+          [sg.Input(key='-Sentence-')],
+          [sg.Button('上传语录', key='-Upload-')],
+          [sg.Text(size=(40,1), key='-SentenceText-')]]
 
 # Create the window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Nya-WSL | 语录上传系统', layout, web_port=5555)
 
 # Display and interact with the Window using an Event Loop
 while True:
     event, values = window.read()
-    # See if user wants to quit or window was closed
+    # See if window was closed
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
+    if event == '-Upload-':
+        Upload()
     # Output a message to the window
-    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
+    window['-SentenceText-'].update('成功将' + values['-SentenceAuthor-'] + '说的' + values['-Sentence-'] + '上传至' + values['-SentenceName-'] + '语录')
 
 # Finish up by removing from the screen
 window.close()
